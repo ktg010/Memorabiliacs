f178b2a3e5dea327514b84636c8bc21d09984d7f
ktg010 <kgilpin2022@gmail.com>
2026-05-02 10:48:58 -0500

    Fixing display of fields in 'view more' for custom items.

--- pages/collectionView.py
+++ pages/collectionView.py
@@ -196,8 +196,11 @@ else:
                 with st_yled.badge_card_one(title=items[item]['info']["Name"], text=field_text, badge_text="Attributes", width="stretch", badge_color="primary", background_color=gfuncs.read_config_val( "backgroundColor"), card_shadow=True, border_style="solid", border_color=gfuncs.read_config_val( "textColor"), border_width=1):
                     for key in items[item]['info'].keys():
                         if key not in ("Name", "Image", 'id'):
-                            if views[key]:
+                            if coll_type == "Custom":
                                 st.write(f"**{key}**: **{items[item]['info'][key]}**")
+                            else:
+                                if views[key]:
+                                    st.write(f"**{key}**: **{items[item]['info'][key]}**")
                     st.divider()
                     st.header(_("Personal Fields"))
                     notes = items[item].get("notes")
